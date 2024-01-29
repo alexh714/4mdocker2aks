c31315c507558b1e89aaaeb0f2f280db106952a4
alexh714 <alexh714@gmail.com>
2024-01-28 19:28:42 -0800

    addeding console service

---
+++ CommentsWebAPIDatabaseSynchronization/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CommentsWebAPIDatabaseSynchronization.Models
+{
+    public class Comment
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("postId")]
+        public int PostId { get; set; }
+        [BsonElement("parentId")]
+        public int ParentId { get; set; }
+        [BsonElement("votes")]
+        public int Votes { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; } = string.Empty;
+        [BsonElement("email")]
+        public string Email { get; set; } = string.Empty;
+        [BsonElement("body")]
+        public string Body { get; set; } = string.Empty;
+    }
+}
---
+++ CommentsWebAPIDatabaseSynchronization/Program.cs
@@ -0,0 +1,36 @@
+﻿using StackExchange.Redis;
+using MongoDB.Driver;
+using CommentsWebAPIDatabaseSynchronization.Models;
+using System.Text.Json;
+using MongoDB.Bson;
+namespace CommentWebAPIDatabaseSynchronization
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("starting...\n");
+            string ConnectionString = "mongodb://mongo:27017";
+            var mongoClient = new MongoClient(ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase("Popularity");
+            IMongoCollection<Comment> commentsCollection = mongoDatabase.GetCollection<Comment>("Comments");
+
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("redis:6379");
+            IDatabase db = redis.GetDatabase();
+
+            foreach (var k in redis.GetServer("redis",6379).Keys()) 
+            {
+                string key = k.ToString();
+                var value = await db.StringGetAsync(key);
+                Comment comment = JsonSerializer.Deserialize<Comment>(value.ToString());
+                if (comment != null)
+                {       
+                    var filter = Builders<Comment>.Filter.Eq("_id", new ObjectId(comment.Id));
+                    var res = await commentsCollection.ReplaceOneAsync(filter, comment);
+                    Console.WriteLine($"{res} for {comment.Id}");
+                }
+            }
+            //Console.WriteLine("closing");
+        }
+    }
+}
